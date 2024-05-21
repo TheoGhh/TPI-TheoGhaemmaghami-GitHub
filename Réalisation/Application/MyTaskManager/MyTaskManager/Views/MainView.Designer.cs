ff5c8d76873954c65a2801989bed13fdd5d15724
TheoGhh <theo_ghaemmaghami@yahoo.com>
2024-05-21 14:44:25 +0200

    Suite de l'interface principale + Fonctionnalité Changement Titre des colonnes
    jdt pas à jour !

--- Réalisation/Application/MyTaskManager/MyTaskManager/Views/MainView.Designer.cs
+++ Réalisation/Application/MyTaskManager/MyTaskManager/Views/MainView.Designer.cs
@@ -29,38 +29,95 @@
         private void InitializeComponent()
         {
             this.pnlNavBar = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
             this.pbxLogo = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
             this.pnlNavBar.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pbxLogo)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // pnlNavBar
             // 
-            this.pnlNavBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pnlNavBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
             this.pnlNavBar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(207)))), ((int)(((byte)(0)))));
+            this.pnlNavBar.Controls.Add(this.button1);
             this.pnlNavBar.Controls.Add(this.pbxLogo);
-            this.pnlNavBar.Location = new System.Drawing.Point(-1, 0);
+            this.pnlNavBar.Location = new System.Drawing.Point(0, 0);
             this.pnlNavBar.Margin = new System.Windows.Forms.Padding(0);
             this.pnlNavBar.Name = "pnlNavBar";
-            this.pnlNavBar.Size = new System.Drawing.Size(975, 129);
+            this.pnlNavBar.Size = new System.Drawing.Size(160, 623);
             this.pnlNavBar.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(12, 167);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(133, 37);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Ajouter une colonne";
+            this.button1.UseVisualStyleBackColor = false;
             // 
             // pbxLogo
             // 
+            this.pbxLogo.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.pbxLogo.Image = global::MyTaskManager.Properties.Resources.TPI___MyTaskManager___Logo;
-            this.pbxLogo.Location = new System.Drawing.Point(407, 3);
+            this.pbxLogo.Location = new System.Drawing.Point(0, 0);
             this.pbxLogo.Name = "pbxLogo";
-            this.pbxLogo.Size = new System.Drawing.Size(125, 120);
+            this.pbxLogo.Size = new System.Drawing.Size(160, 129);
             this.pbxLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pbxLogo.TabIndex = 7;
             this.pbxLogo.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Location = new System.Drawing.Point(202, 26);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(171, 582);
+            this.panel1.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(3, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 20);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "A FAIRE";
+            this.label1.Click += new System.EventHandler(this.lblTitle_Click);
+            this.label1.MouseLeave += new System.EventHandler(this.lblTitle_MouseLeave);
+            this.label1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.lblTitle_MouseUp);
+            // 
+            // panel2
+            // 
+            this.panel2.AllowDrop = true;
+            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.panel2.AutoScroll = true;
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Location = new System.Drawing.Point(3, 40);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(163, 539);
+            this.panel2.TabIndex = 9;
             // 
             // MainView
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(971, 588);
+            this.ClientSize = new System.Drawing.Size(1165, 620);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.pnlNavBar);
             this.Name = "MainView";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -68,6 +125,8 @@
             this.Load += new System.EventHandler(this.MainView_Load);
             this.pnlNavBar.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pbxLogo)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -76,5 +135,9 @@
 
         private System.Windows.Forms.Panel pnlNavBar;
         private System.Windows.Forms.PictureBox pbxLogo;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
     }
 }
--- Réalisation/Application/MyTaskManager/MyTaskManager/Views/MainView.cs
+++ Réalisation/Application/MyTaskManager/MyTaskManager/Views/MainView.cs
@@ -12,6 +12,8 @@ namespace MyTaskManager.Views
 {
     public partial class MainView : Form
     {
+        private TextBox tbxEditTitle = new TextBox();
+
         public MainView()
         {
             InitializeComponent();
@@ -19,10 +21,51 @@ namespace MyTaskManager.Views
 
         private void MainView_Load(object sender, EventArgs e)
         {
-            pbxLogo.Location = new Point(pnlNavBar.Width-Width / 2, pnlNavBar.Height / 2);
+            tbxEditTitle.Size = label1.Size;
+            tbxEditTitle.Location = label1.Location;
+            tbxEditTitle.Hide();
+            tbxEditTitle.Leave += EditTextBox_Leave; // Gère la perte de focus
+            tbxEditTitle.KeyPress += EditTextBox_KeyPress; // Gère la pression sur la touche Entrée
+
+        }
+        private void EditTextBox_Leave(object sender, EventArgs e)
+        {
+            UpdateLabel(); // Met à jour le label quand le TextBox perd le focus
+        }
+
+        private void EditTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Vérifie si la touche Entrée est pressée
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                UpdateLabel(); // Met à jour le label et cache le TextBox
+                e.Handled = true; // Indique que l'événement de touche est géré
+            }
+        }
+        private void UpdateLabel()
+        {
+            label1.Text = tbxEditTitle.Text; // Met à jour le texte du label
+            tbxEditTitle.Hide(); // Cache le TextBox
+        }
+
+        private void lblTitle_Click(object sender, EventArgs e)
+        {
+            tbxEditTitle.Text = label1.Text;
+            tbxEditTitle.Show();
+            tbxEditTitle.Focus();
+        }
+
+        private void lblTitle_MouseUp(object sender, MouseEventArgs e)
+        {
+            label1.ForeColor = Color.Gray;
+            this.Cursor = Cursors.Hand;
             
         }
 
-       
+        private void lblTitle_MouseLeave(object sender, EventArgs e)
+        {
+            this.Cursor = Cursors.Default;
+            label1.ForeColor = Color.Black;
+        }
     }
 }
